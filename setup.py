2c4bf04cae2c06f8949eaf61bed5251caff9aa43
Daniele Di Sarli <danieleds0@gmail.com>
2015-05-27 10:05:51 +0200

    Setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='notepadqqapi',
+      version='0.9',
+      description='Notepadqq API Layer for extensions',
+      author='Daniele Di Sarli',
+      author_email='danieleds0@gmail.com',
+      url='http://notepadqq.altervista.org/',
+      packages=['notepadqq_api'],
+     )
